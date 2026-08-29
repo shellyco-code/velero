c9f52a02589853693793d2832595e543f29d86e0
shellyco-code <shellyco-code@users.noreply.github.com>
2026-08-29 11:53:42 +0530

    tests: remove BOM from test files and use assert.Empty for empty string check
    Signed-off-by: shellyco-code <shellyco-code@users.noreply.github.com>

--- pkg/util/boolptr/boolptr_test.go
+++ pkg/util/boolptr/boolptr_test.go
@@ -1,26 +1,27 @@
-﻿package boolptr
+package boolptr
 
 import (
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestIsSetToTrue(t *testing.T) {
 	assert.False(t, IsSetToTrue(nil))
-	
+
 	f := false
 	assert.False(t, IsSetToTrue(&f))
-	
+
 	tr := true
 	assert.True(t, IsSetToTrue(&tr))
 }
 
 func TestIsSetToFalse(t *testing.T) {
 	assert.False(t, IsSetToFalse(nil))
-	
+
 	tr := true
 	assert.False(t, IsSetToFalse(&tr))
-	
+
 	f := false
 	assert.True(t, IsSetToFalse(&f))
 }
--- pkg/util/stringptr/stringptr_test.go
+++ pkg/util/stringptr/stringptr_test.go
@@ -1,16 +1,17 @@
-﻿package stringptr
+package stringptr
 
 import (
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGetString(t *testing.T) {
 	assert.Equal(t, NilString, GetString(nil))
-	
+
 	val := "hello"
 	assert.Equal(t, "hello", GetString(&val))
-	
+
 	empty := ""
-	assert.Equal(t, "", GetString(&empty))
+	assert.Empty(t, GetString(&empty))
 }
